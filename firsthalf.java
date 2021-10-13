51b98322575d07b84c29aa79a8d64d6f26ee9477
Sree vidyadhari-221810309061 <52527880+Vidyadhari77@users.noreply.github.com>
2021-10-13 13:26:42 +0530

    Add files via upload

---
+++ firsthalf.java
@@ -0,0 +1,15 @@
+public import java.util.*;
+import java.lang.string;
+public class firsthalf
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the string");
+        String s=sc.next();
+        String[] words=s.split("//s");
+        System.out.println(words[0]);
+    }
+}
+    
+
