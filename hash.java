5293615c311c84294254733d814fc32521555dc9
Sree vidyadhari-221810309061 <52527880+Vidyadhari77@users.noreply.github.com>
2021-10-13 13:28:14 +0530

    Add files via upload

---
+++ hash.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class hashvalue {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s1= sc.next();
+        String s2= sc.next();
+        System.out.println("string s1 hashcode"+s1.hashCode());
+        System.out.println("String s2 hashcode"+s2.hashCode());
+    }
+    
+}
